0efccc0d9cc968d303a5ab26a602304eb1eaf234
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-08-23 17:08:34 +0200

    fixed the App Component 3d test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-app');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
-  // });
+  it('should render the frontend welcome message', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1#frontEndMsg')?.textContent).toContain('Hello from Frontend !');
+  });
 });
