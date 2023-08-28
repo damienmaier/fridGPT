1ec75f18a7a7e75c02ffe4747f78810c5eeb602e
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-08-28 08:19:42 +0000

    tests for recipe endpoint

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,9 +1,43 @@
-from unittest import TestCase
+import unittest
+
+import main
 
 
-class Test(TestCase):
-    def test_ok(self):
-        self.assertTrue(True)
+class ApiEndpointTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = main.create_app()
+        self.client = self.app.test_client()
 
-    # def test_nok(self):
-    #     self.fail()
+
+@unittest.skip("Not implemented")
+class RecipeEndpointTest(ApiEndpointTest):
+    JSON_RECIPE_REQUEST = '''
+            "ingredients": [
+                "carottes",
+                "pommes de terre",
+                "poireaux",
+                "oignons",
+                "beurre",
+                "lardons",
+            ]
+            '''
+
+    JSON_WRONG_RECIPE_REQUEST = '''
+            "ingredients": []
+            '''
+
+    def test_should_return_200_for_correct_request(self):
+        response = self.client.post('/recipe', json=self.JSON_RECIPE_REQUEST)
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_return_400_for_wrong_request(self):
+        response = self.client.post('/recipe', json=self.JSON_WRONG_RECIPE_REQUEST)
+        self.assertEqual(response.status_code, 400)
+
+    def test_should_return_dish_description(self):
+        response = self.client.post('/recipe', json=self.JSON_RECIPE_REQUEST)
+        self.assertIsInstance(response.json['dishDescription'], str)
+
+    def test_should_return_recipe(self):
+        response = self.client.post('/recipe', json=self.JSON_RECIPE_REQUEST)
+        self.assertIsInstance(response.json['recipe'], str)
