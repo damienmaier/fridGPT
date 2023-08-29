aeab5f8b2b240f9c4aa350edb3e27e55163a97e5
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-08-29 19:31:50 +0200

    frontend - fixed tests

--- frontend/src/tests/fake-services.ts
+++ frontend/src/tests/fake-services.ts
@@ -1,11 +1,11 @@
 import { Subject } from "rxjs";
-import { BaseIngredient } from "src/app/models/ingredient";
+import { Ingredient } from "src/app/models/ingredient";
 import { Recipe } from "src/app/models/recipe";
 import { RecipesService } from "src/app/services/recipes.service";
 
 function createRecipesServiceSpy() {
     const spyObj              = jasmine.createSpyObj<RecipesService>('RecipesService', ['loadIngredients', 'loadRecipe', 'recipeIsLoading']);
-    spyObj.ingredientsSubject = new Subject<BaseIngredient[]>();
+    spyObj.ingredientsSubject = new Subject<Ingredient[]>();
     spyObj.recipeSubject      = new Subject<Recipe>();
     return spyObj;
 }
