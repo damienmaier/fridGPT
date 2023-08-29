24486575a7336e3c7c81ec2310c88233e2727eba
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-08-29 14:37:32 +0200

    frontend - added image loaded from API

--- frontend/src/app/services/recipes.service.ts
+++ frontend/src/app/services/recipes.service.ts
@@ -3,6 +3,7 @@ import { BaseIngredient, Ingredient } from "../models/ingredient";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../models/recipe";
+import { DishImage } from "../models/image";
 
 @Injectable()
 export class RecipesService {
@@ -52,19 +53,36 @@ export class RecipesService {
 
     loadRecipe(selection: Ingredient[]): void {
         this.currentlyLoadingRecipe = true;
+        this.recipe                 = {dishDescription: '', instructions: '', image: { url: ''}};
         const params = {ingredients: selection.map((e) => e.baseIngredient.name)};
         this.http.post<Recipe>('/api/recipe', params).subscribe({
             next: (sentRecipe: Recipe) => {
               this.recipe = sentRecipe;
-              this.currentlyLoadingRecipe = false;
-              this.recipeSubject.next(this.recipe);
+              // we load now the image
+              this.http.post<DishImage>('/api/image', {dishDescription: this.recipe.dishDescription}).subscribe({
+                next: (image: DishImage) => {
+                    this.recipe.image = image;
+                    this.emitRecipe();
+                },
+                error: (e) => {
+                    console.log(e); // todo: better error management
+                    this.emitRecipe(true);
+                }
+              });
             },
             error: (e) => {
-                this.currentlyLoadingRecipe = false;
-                this.recipe = {dishDescription: 'no data loaded', instructions: '', imageURL: ''};
-                this.recipeSubject.next(this.recipe);
+                console.log(e); // todo: better error management
+                this.emitRecipe(true);
             }
         });
+    }
+
+    private emitRecipe(error: boolean = false) {
+        if(error) {
+            this.recipe.dishDescription = 'no data loaded';
+        }
+        this.recipeSubject.next(this.recipe);
+        this.currentlyLoadingRecipe = false;
     }
 
     recipeIsLoading(): boolean {
