abeb402fcceaca380e1668546f9a665a80de67eb
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-08-30 13:36:51 +0000

    update ingredients endpoint tests

--- backend/test_api.py
+++ backend/test_api.py
@@ -192,14 +192,23 @@ class ImageEndpointTest(ApiEndpointTest):
                         'Should return an image url')
 
 
+@unittest.skip("Not implemented")
 class IngredientsEndpointTest(ApiEndpointTest):
 
-    def test_a(self):
+    def test_result(self):
         response = self.client.get('/api/ingredients')
 
         self.assertEqual(response.status_code, 200, 'Should return 200')
 
-        with open(main.PROJECT_ROOT_PATH / 'data' / 'ingredients_fr.json', 'r', encoding='utf-8') as file:
-            ingredients = json.load(file)
-        response = self.client.get('/api/ingredients')
-        self.assertEqual(response.json, ingredients, 'Should return ingredients list')
+        self.assertIsInstance(response.json['ingredients'], list, 'Should return ingredients list')
+        for ingredient in response.json['ingredients']:
+            self.assertIsInstance(ingredient["name"], str, 'Should return ingredient name')
+            self.assertTrue(3 <= len(ingredient["name"]) <= 50, 'Should return ingredient name of correct length')
+
+            self.assertIsInstance(ingredient["unit"], str, 'Should return ingredient unit')
+            self.assertTrue(1 <= len(ingredient["unit"]) <= 50, 'Should return ingredient unit of correct length')
+
+            self.assertIsInstance(ingredient["defaultQuantity"], float, 'Should return ingredient quantity')
+            self.assertTrue(0 < ingredient["defaultQuantity"], 'Should return positive ingredient quantity')
+
+            self.assertIsInstance(ingredient["autoAdd"], bool, 'Should return ingredient auto add status')
