81c51543ccadbf314fcdbbc30285fbae9aa43d19
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-08-31 19:53:32 +0000

    sys exit tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,7 +1,9 @@
+import sys
 import unittest
 
 import root
 
 if __name__ == '__main__':
     test_suite = unittest.defaultTestLoader.discover(start_dir=root.PROJECT_ROOT_PATH, pattern='*_test.py')
-    unittest.TextTestRunner(verbosity=2).run(test_suite)
+    test_result = unittest.TextTestRunner(verbosity=2).run(test_suite)
+    sys.exit(not test_result.wasSuccessful())
