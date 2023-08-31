af5f856d7e41e1e7536a09901043356e540c13ee
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-08-31 11:06:43 +0200

    frontend - small fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
-import { LoadingComponent } from './components/loading/loading.component';
 
 const routes: Routes = [
-    { path: 'app', component: LoadingComponent },
+    { path: 'app', component: SearchComponent },
     { path: 'app/recipe', component: RecipeComponent },
     { path: '**', redirectTo: 'app' }
   ];
