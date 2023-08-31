5794a72c85231a4edfc5b9e856ec453476cc5db3
celestinPiccin <celestin.piccin@heig-vd.ch>
2023-08-31 18:49:28 +0200

    big recipe prompt, first version

--- backend/gpt/task.py
+++ backend/gpt/task.py
@@ -10,10 +10,10 @@ class GptAssistedTask(abc.ABC):
     def __call__(self, *args, **kwargs):
         prompt = self.build_gpt_prompt(*args, **kwargs)
         gpt_response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
+            model=self.model(),
             messages=prompt.messages,
             temperature=self.temperature(),
-            max_tokens=1000,
+            max_tokens=self.max_tokens(),
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
@@ -23,6 +23,13 @@ class GptAssistedTask(abc.ABC):
     def temperature(self):
         return 1
 
+    def model(self):
+        return 'gpt-3.5-turbo'
+
+    def max_tokens(self):
+        return 1000
+
+    
     @abc.abstractmethod
     def build_gpt_prompt(self, *args, **kwargs) -> gpt.prompt.Prompt:
         pass
