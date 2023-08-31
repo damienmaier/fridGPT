228a36b549e160a58be2154fc6539f036353c8cf
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-08-31 19:34:09 +0000

    check failing test

--- backend/api_test.py
+++ backend/api_test.py
@@ -33,7 +33,7 @@ class RecipeEndpointTest(ApiEndpointTest):
         self.assertEqual(response.status_code, 400, 'Should return 400')
         self.assertEqual(
             response.json,
-            {"error": "malformed request"},
+            {"error": "malformed requests"},
             'Should return malformed request error'
         )
 
