7e11482b65ba40e5c93252db6c8def757098d0c6
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-08-31 10:36:40 +0200

    frontend - improved quantity display

--- frontend/src/app/models/ingredient.ts
+++ frontend/src/app/models/ingredient.ts
@@ -16,10 +16,11 @@ export class IngredientForRecipe {
     isCustom: boolean // generated & used by frontend only
     name!: string;
     quantity!: IngredientQuantity;
-    withQuantity: boolean = true;
+    withQuantity!: boolean;
     constructor(ingredient: Ingredient) {
-        this.quantity   = {unit: ingredient.unit, value: ingredient.defaultQuantity};
-        this.name       = ingredient.name;
-        this.isCustom   = ingredient.isCustom;
+        this.quantity       = {unit: ingredient.unit, value: ingredient.defaultQuantity};
+        this.name           = ingredient.name;
+        this.isCustom       = ingredient.isCustom;
+        this.withQuantity   = false;
     }
 }
