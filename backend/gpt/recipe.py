4a834db365793e9a88fe5fbd1a22470689f388af
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-09-01 14:06:56 +0000

    use notmal gpt turbo

--- backend/gpt/recipe.py
+++ backend/gpt/recipe.py
@@ -9,7 +9,7 @@ import root
 class GptAssistedRecipeFinder(gpt.task.GptAssistedTask):
 
     def __init__(self):
-        super().__init__(model='gpt-3.5-turbo-16k', max_tokens=12500)
+        super().__init__(max_tokens=3000)
 
     def build_gpt_prompt(self, ingredients: [models.RequestedIngredient]):
 
