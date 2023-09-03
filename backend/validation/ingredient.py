ee1bc9c25a26f72fcd0ceff88c7ffa7c249a8fb2
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-09-03 15:49:16 +0000

    validation logs

--- backend/validation/ingredient.py
+++ backend/validation/ingredient.py
@@ -1,15 +1,27 @@
-import errors
+import config
+
 import data
+import errors
 from . import classifiers
+
+logger = config.logging.getLogger(__name__)
 
 
 def parse_and_validate_ingredients(json_request) -> [data.RequestedIngredient]:
+
     if not json_request['ingredients']:
+        logger.error('received empty ingredients list')
         raise errors.MalformedRequestError
+
     if len(json_request['ingredients']) > 100:
+        logger.error('received too many ingredients')
         raise errors.TooManyIngredientsError
+
     ingredients = [_parse_and_validate_ingredient(ingredient) for ingredient in json_request['ingredients']]
+
+    logger.info('checking if sufficient ingredients were received')
     if not classifiers.is_sufficient_ingredients(ingredients):
+        logger.warning('insufficient ingredients')
         raise errors.InsufficientIngredients
 
     return ingredients
@@ -19,19 +31,33 @@ def _parse_and_validate_ingredient(json_ingredient) -> data.RequestedIngredient:
     try:
         ingredient = data.RequestedIngredient.from_dict(json_ingredient)
     except (TypeError, KeyError):
+        logger.error('malformed request')
         raise errors.MalformedRequestError
+
+    logger.info(f'validating ingredient {ingredient.name}')
 
     if ingredient.name in data.SUGGESTED_INGREDIENTS:
         if ingredient.quantity and ingredient.quantity.unit != data.SUGGESTED_INGREDIENTS[ingredient.name].unit:
+            logger.error(f'received wrong unit for ingredient {ingredient.name} which is in suggested ingredients')
             raise errors.WrongIngredientUnitError(ingredient)
+
     else:
+
+        logger.info(f'validating custom ingredient {ingredient.name} length')
         if not 0 < len(ingredient.name) <= 50:
+            logger.warning(f'custom ingredient {ingredient.name} has invalid length')
             raise errors.InvalidCustomIngredientError(ingredient)
+
+        logger.info(f'validating custom ingredient {ingredient.name} name')
         if not classifiers.is_valid_ingredient(ingredient.name):
+            logger.warning(f'custom ingredient {ingredient.name} is invalid')
             raise errors.InvalidCustomIngredientError(ingredient)
+
+        logger.info(f'validating custom ingredient {ingredient.name} unit')
         if ingredient.quantity:
             if not classifiers.is_valid_unit_for_ingredient(ingredient_name=ingredient.name,
                                                             unit=ingredient.quantity.unit):
+                logger.warning(f'custom ingredient {ingredient.name} has invalid unit {ingredient.quantity.unit}')
                 raise errors.InvalidCustomIngredientUnitError(ingredient)
 
     return ingredient
