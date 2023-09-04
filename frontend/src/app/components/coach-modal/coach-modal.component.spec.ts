a938449d12ffaba86f51b5801d5446c71b1267d3
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-09-04 18:34:31 +0200

    frontend - fixed tests

--- frontend/src/app/components/coach-modal/coach-modal.component.spec.ts
+++ frontend/src/app/components/coach-modal/coach-modal.component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoachModalComponent } from './coach-modal.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('CoachModalComponent', () => {
   let component: CoachModalComponent;
   let fixture: ComponentFixture<CoachModalComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [CoachModalComponent]
-    });
-    fixture = TestBed.createComponent(CoachModalComponent);
-    component = fixture.componentInstance;
+      declarations: [CoachModalComponent],
+      providers:    [NgbActiveModal]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture    = TestBed.createComponent(CoachModalComponent);
+    component  = fixture.componentInstance;
+    component.coach = {name: '', description: '', imageUrl:''}
     fixture.detectChanges();
   });
 
