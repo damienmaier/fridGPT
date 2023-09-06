10a2ef8142be14ca0a8166ea8f336308664de57e
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-09-06 22:55:55 +0200

    frontend - fixed recipe component

--- frontend/src/app/components/recipe/recipe.component.ts
+++ frontend/src/app/components/recipe/recipe.component.ts
@@ -18,31 +18,14 @@ export class RecipeComponent {
   constructor(private recipeService: RecipesService, private route: ActivatedRoute, private modalService: ModalService) {}
 
   ngOnInit() {
-    this.recipe = {
-      "coach": {
-          "description": "Germaine, une retrait\u00e9e pleine de vie, enchante sa cuisine de couleurs et de sourires en concoctant des d\u00e9lices pour ses petits-enfants. \nToujours \u00e0 la recherche de nouvelles recettes, elle transforme chaque repas en une aventure gourmande, pr\u00eate \u00e0 rire de ses \u00e9checs et \u00e0 partager des moments savoureux avec sa famille.\nSa cuisine est un voyage joyeux o\u00f9 chaque plat raconte une histoire et chaque bouch\u00e9e r\u00e9v\u00e8le sa passion pour faire plaisir aux autres.",
-           "imageUrl": "/assets_app/germaine.png","name": "Germaine"
-      },
-      "dishDescription": "Un d\u00e9licieux plat traditionnel fran\u00e7ais \u00e0 base de b\u0153uf, de vin rouge et d'\u00e9pices.",
-      "dishName": "Boeuf bourguignon",
-      "ingredients": "- 500g de b\u0153uf\n- 50g de farine\n- 1 cuill\u00e8re \u00e0 caf\u00e9 de poivre\n- 1 cuill\u00e8re \u00e0 caf\u00e9 de sel\n- 1 cuill\u00e8re \u00e0 soupe de sucre\n- Huile d'olive\n- Huile de cuisson\n- Vinaigre\n- Armoire \u00e0 \u00e9pices compl\u00e8te",
-      "steps": [
-          "1. Couper le b\u0153uf en cubes de taille moyenne.","2. Dans un sac plastique, m\u00e9langer la farine, le poivre, le sel et le sucre.",
-      ],
-      imageUrl: '/assets_app/empty.jpg'
-    };
+    const index             = this.route.snapshot.paramMap.get('recipeId');
+    this.recipe             = this.recipeService.getRecipe(index == null ? -1 : +index);
     this.currentStep        = this.recipe.ingredients;
     this.currentStepIndex   = this.recipe.steps.length;
+    if(this.recipe.dishName === '') {
+      this.recipeService.goToHome(); // no recipes loaded, go back to home screen
+    }
     this.recipe.ingredients = this.recipe.ingredients.replaceAll('\n', '<br>'); 
-
-    // const index             = this.route.snapshot.paramMap.get('recipeId');
-    // this.recipe             = this.recipeService.getRecipe(index == null ? -1 : +index);
-    // this.currentStep        = this.recipe.ingredients;
-    // this.currentStepIndex   = this.recipe.steps.length;
-    // if(this.recipe.dishName === '') {
-    //   this.recipeService.goToHome(); // no recipes loaded, go back to home screen
-    // }
-    // this.recipe.ingredients = this.recipe.ingredients.replaceAll('\n', '<br>'); 
   }
 
   nextStep(forward: boolean) {
