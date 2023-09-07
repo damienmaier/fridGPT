53504beef04436669dec717dbda655a58b37f080
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2023-09-07 21:02:08 +0200

    dalle forbidden words quickfix

--- backend/ai/dalle.py
+++ backend/ai/dalle.py
@@ -3,9 +3,18 @@ from config import openai
 
 logger = config.logging.getLogger(__name__)
 
+FORBIDDEN_STRINGS = ['râper', 'raper', 'rape', 'râpe', 'rapé', 'râpé']
+
 
 def create_image(prompt, size='512x512') -> str:
     logger.info(f'Creating image for prompt: {prompt}')
+
+    if any(forbidden_string in prompt for forbidden_string in FORBIDDEN_STRINGS):
+        logger.warning(f'Removing forbidden strings from prompt: {FORBIDDEN_STRINGS}')
+
+        for forbidden_string in FORBIDDEN_STRINGS:
+            prompt = prompt.replace(forbidden_string, '')
+
     image_url = openai.Image.create(prompt=prompt, n=1, size=size)['data'][0]['url']
     logger.info(f'Image created: {image_url}')
 
